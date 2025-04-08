7c8bb19c354913991d4668a6ff185d8184dec404
yenfu <alex7008uk@gmail.com>
2025-04-08 09:19:09 +0800

    chore(leaflet): 移除 leaflet draw 相關程式碼

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import '@/assets/normalize.css'
 import 'leaflet/dist/leaflet.css'
-import 'leaflet-draw/dist/leaflet.draw.css'
 
 import App from './App.vue'
 import router from './router'
